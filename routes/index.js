15a8f0fb5f28e686bfb889f92c448e1100a12bac
Ganeshk750 <ganesh9ot@gmail.com>
2019-11-21 23:56:52 +0530

    adding routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-
+/* All Authors Route */
 router.get('/', (req,res) =>{
-   res.render('index')
+   res.render('authors/new')
+});
+/* New Authors Route */
+router.get('/', (req,res) =>{
+   res.render('authors/new')
+});
+/* Create Authors Route */
+router.post('/', (req,res) =>{
+   res.render('Create')
 });
 
 module.exports = router;
